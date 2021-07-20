3a756be083ee42fba46eb673adc8207a2cf87291
taiki <turkey.31452@gmail.com>
2021-07-20 16:12:31 +0900

    シフトデータの読み込み

--- transform.py
+++ transform.py
@@ -5,3 +5,10 @@ outputPath = "./output/shft.csv"
 with open(outputPath,'w') as f:
     writer = csv.writer(f)
     writer.writerow(["Subject","Start Date","Start Time","End Date","End Time"])
+
+
+inputPath = "./data/shift.txt"
+with open(inputPath,'r') as f:
+    dataString = f.read()
+
+

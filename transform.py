05cf2f1adc034e2c098249f9ceb555373217eb90
taiki <turkey.31452@gmail.com>
2021-07-20 15:58:20 +0900

    pathを修正

--- transform.py
+++ transform.py
@@ -2,6 +2,6 @@ import csv
 
 outputPath = "./output/shft.csv"
 
-with open(filepath + filename,'w') as f:
+with open(outputPath,'w') as f:
     writer = csv.writer(f)
     writer.writerow(["Subject","Start Date","Start Time","End Date","End Time"])

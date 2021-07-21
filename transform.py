6e1fd3b2bc5b263c717d632e7ac41c8ab46affaf
taiki <turkey.31452@gmail.com>
2021-07-21 13:35:27 +0900

    正規表現を用いて入力データをチェックするcheckData()を追加

--- transform.py
+++ transform.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import re
 
 outputPath = "./output/shft.csv"
 
@@ -13,19 +14,62 @@ with open(inputPath, 'r') as f:
     dataString = f.read()
 
 
-class formatter:
+class Formatter:
     def __init__(self, dataString):
         self.dataDicts = []
         dataArray = dataString.splitlines()
         self.parseData([dataArray[0],dataArray[1]])
         self.dataDicts.append(self.parseData([dataArray[0],dataArray[1]]))
-
-    def parseData(self, dataString):
-        '''データを一回分もらい、辞書形式に直して渡す
+    
+    def checkData(self,dataString):
+        '''それがシフトボードから出力されたデータなのかを正規表現を用いてチェックする
 
         Parameters
         ----------
         dataString : String
+            文字列データの一行分
+
+        Returns
+        -------
+        int
+            データかどうか
+            0 : データではない
+            1 : 時間データである
+            2 : バイト先データである
+            3 : 何もない
+            4 : 最終行のデータである
+        '''
+
+        NOT_SHIFT_DATA = 0
+        TIME_DATA = 1
+        SUBJECT_DATA = 2
+        NO_DATA = 3
+        END_LINE = 4
+        stateData = NOT_SHIFT_DATA
+        
+        timeRe = re.compile("\d{2}/\d{2}（(月|火|水|木|金|土|日)）\d{2}:\d{2}\s-\s\d{2}:\d{2}")
+        subRe = re.compile("-\s.+")
+
+        if timeRe.fullmatch(dataString) is not None:
+            stateData = TIME_DATA
+        elif subRe.fullmatch(dataString) is not None:
+            stateData = SUBJECT_DATA
+        elif len(dataString) == 0:
+            stateData = NO_DATA
+        elif dataString == "シフト管理アプリ「シフトボード」で作成":
+            stateData = END_LINE
+
+        return stateData
+
+        
+
+
+    def parseData(self, dataStrings):
+        '''データを一回分もらい、辞書形式に直して渡す
+
+        Parameters
+        ----------
+        dataStrings : String
             一回分のデータ
             最初の列に日付、次の列にバイト先の名前が入っている
 
@@ -35,9 +79,9 @@ class formatter:
             バイト先の名前、始まりの時間、終わりの時間
         '''
         dataDict = {}
-        dataDict["startDate"] = self.setDate(dataString[0][0:2],dataString[0][3:5],dataString[0][8:10],dataString[0][11:13])
-        dataDict["endDate"] = self.setDate(dataString[0][0:2],dataString[0][3:5],dataString[0][16:18],dataString[0][19:21])
-        dataDict["subject"] = dataString[1][2:]
+        dataDict["startDate"] = self.setDate(dataStrings[0][0:2],dataStrings[0][3:5],dataStrings[0][8:10],dataStrings[0][11:13])
+        dataDict["endDate"] = self.setDate(dataStrings[0][0:2],dataStrings[0][3:5],dataStrings[0][16:18],dataStrings[0][19:21])
+        dataDict["subject"] = dataStrings[1][2:]
         return dataDict
     
     @staticmethod
@@ -75,4 +119,4 @@ class formatter:
 
 
 
-c = formatter(dataString)
+c = Formatter(dataString)

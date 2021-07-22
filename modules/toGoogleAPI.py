25f947609bdf5fb4e345e613d695d4cf206b05db
taiki <turkey.31452@gmail.com>
2021-07-22 22:00:50 +0900

    ディレクトリを整理

--- modules/toGoogleAPI.py
+++ modules/toGoogleAPI.py
@@ -18,6 +18,7 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    # token.jsonというファイルには、ユーザーのアクセストークンとリフレッシュトークンが格納されています。自動的に作成されます。
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.

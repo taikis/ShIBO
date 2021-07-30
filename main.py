48e2bcc613ff4bb2b99afc80434ef16fa7b147cc
taiki <turkey.31452@gmail.com>
2021-07-31 00:46:09 +0900

    kiviのレイアウトを変更

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import csv
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+from kivy.uix.widget import Widget
 
 from modules import shibo
 from modules import toGoogle
+
+resource_add_path("./fonts/")
+LabelBase.register(DEFAULT_FONT, "ipaexg.ttf")
 
 inputPath = "./data/shift.txt"
 with open(inputPath, 'r') as f:
@@ -13,6 +19,13 @@ with open(inputPath, 'r') as f:
 c = shibo.ShiftContainer(dataString)
 event_dicts = c.dataDicts
 c.toGoogleCSV()
+
+
+class LoadCalendar(Widget):
+    def googleBotton(self):
+        toGoogle.setEvent(event_dicts)
+
+
 
 class ShiboApp(App):
     pass
